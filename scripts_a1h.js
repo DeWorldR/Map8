e8d0736561000adeb3fa6c0489a51568d047415b
DeWorldR <129869822+DeWorldR@users.noreply.github.com>
2023-08-11 11:15:19 +0700

    Add files via upload

---
+++ scripts_a1h.js
@@ -0,0 +1,66 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuMTIzdGgiLCJhIjoiY2xrcG56cnR1MHoxMjNwa3k3MXdvdGs4ayJ9.OLRvMH5R4M-IlWp5vqMouQ';
+
+// สร้างแผนที่
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [0, 0],
+  zoom: 13
+});
+
+// สร้าง Marker
+var marker = new mapboxgl.Marker()
+  .setLngLat([0, 0])
+  .addTo(map);
+
+  // สร้าง Marker ปลายทาง
+var destinationMarker = new mapboxgl.Marker({ color: '#ff0000' }) // เลือกสีส้มสำหรับเครื่องหมาย
+  .setLngLat([99.82662199434705, 19.902230037379034])
+  .addTo(map);
+
+if ("geolocation" in navigator) {
+  // ขอสิทธิ์ในการรับตำแหน่งปัจจุบันของผู้ใช้
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    map.setCenter([lng, lat]);
+    marker.setLngLat([lng, lat]);
+
+    // ใช้ Marker ปลายทางเพื่อแสดงตำแหน่ง
+    destinationMarker.setLngLat([99.82662199434705, 19.902230037379034]);
+
+
+    var directionsAPI = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + lng + ',' + lat + ';' + '99.82663054208719' + ',' + '19.902518866789627' + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+    fetch(directionsAPI)
+      .then(response => response.json())
+      .then(data => {
+        // แสดงเส้นทางในแผนที่
+        var route = data.routes[0].geometry;
+        map.addLayer({
+          id: 'route',
+          type: 'line',
+          source: {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              properties: {},
+              geometry: route
+            }
+          },
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+          },
+          paint: {
+            'line-color': '#8723cf', 
+            'line-width': 8
+          }
+        });
+      });
+  });
+} else {
+
+  alert("Geolocation is not supported by your browser");
+}
